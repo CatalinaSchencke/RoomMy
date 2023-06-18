d5cdd0807adaed13ec5ce5830fab139a982a3261
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-18 19:05:20 -0400

    add basic options component

---
+++ components/home/Options.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="flex flex-col flex-wrap gap-y-2 lg:flex lg:flex-row lg:justify-center lg:gap-x-8 lg:mx-2 lg:mb-8">
+        <div class=" lg:w-[25%] lg:h-auto  lg:relative grid justify-items-center">
+          <img src="/home/dorm.jpg" alt="" class="hidden lg:block rounded-[40px]">
+          <a href="profiles/offer/part1.html" class="drop-shadow-md flex justify-center lg:absolute lg:top-[82%] bg-white w-[80%] rounded-full lg:py-2" >
+            <button class="text-center  lg:font-medium">
+              Arriendo una <b>habitación</b>
+            </button>
+          </a>
+        </div>
+        <div class=" lg:w-[25%] lg:h-auto lg:relative grid justify-items-center">
+          <img src="/home/roomie.jpg" alt="" class="hidden lg:block rounded-[40px]">
+          <a href="profiles/roomie/part1.html" class="drop-shadow-md flex justify-center lg:absolute lg:top-[82%] bg-white w-[80%] rounded-full lg:py-2" >
+            <button class="text-center lg:font-medium">
+              Busco <b>Roomie</b>
+            </button>
+          </a>
+        </div>
+        <div class=" lg:w-[25%] lg:h-auto lg:relative grid justify-items-center">
+          <img src="home/property.jpg" alt="" class="hidden lg:block rounded-[40px]">
+          <a href="profiles/offer/part1.html" class="drop-shadow-md flex justify-center lg:absolute lg:top-[82%] bg-white w-[80%] rounded-full lg:py-2" >
+            <button class="text-center lg:font-medium">
+              Arriendo una <b>Propiedad</b>
+            </button>
+          </a>  
+        </div>
+    </section>
+</template>
