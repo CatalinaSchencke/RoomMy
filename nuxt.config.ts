f46f3493da5c384f2fcd42dedc6479c3b18496ba
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-18 20:04:51 -0400

    add font awesome script to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'RoomMy',
+      script: [
+        {
+          src: 'https://kit.fontawesome.com/64d58efce2.js',
+          crossorigin: 'anonymous',
+        }
+      ]
+    }
+  }
 })
