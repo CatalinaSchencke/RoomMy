03e11cf7dff76c181be46d48a3f2fe17da8a7243
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-18 20:29:10 -0400

    add scoped to style tag in auth page

--- pages/autenticacion.vue
+++ pages/autenticacion.vue
@@ -85,7 +85,7 @@
         </div>
     </div>
 </template>
-<style>
+<style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
 
