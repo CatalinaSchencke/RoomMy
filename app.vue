596a9eb4152b90f6627f3226ed1f46aa347bcd28
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-25 18:52:59 -0400

    add layout transitions

--- app.vue
+++ app.vue
@@ -4,9 +4,22 @@
   </NuxtLayout>
 </template>
 <style>
-  body {
-    background: url("/background.png");
-    background-size: cover;
-    background-repeat: no-repeat;
-  }
+body {
+  background: url("/background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.13s;
+}
+
+.layout-enter-from {
+  opacity: 0;
+}
+
+.layout-leave-to {
+  opacity: 0;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,10 @@ export default defineNuxtConfig({
           crossorigin: 'anonymous',
         }
       ]
-    }
+    },
+    layoutTransition: {
+      name: 'layout',
+      mode: 'out-in'
+    },
   }
 })
