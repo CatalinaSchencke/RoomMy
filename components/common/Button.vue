574db25d6b4fdac51d845bc66a70203551b8f508
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-28 15:29:38 -0400

    abstract button to a component

---
+++ components/common/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button :type="type"
+        class="cursor-pointer mt-2 px-6 py-2 rounded-full bg-darken-blue disabled:bg-darken-blue/30 text-white disabled:cursor-not-allowed"
+        :disabled="disabled">
+        {{ text }}
+    </button>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    text: {
+        type: String,
+        default: 'Botón',
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+    type: {
+        type: String,
+        default: 'button',
+    }
+});
+
+</script>
