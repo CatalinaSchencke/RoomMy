fd25b67dde394a3a084b43d62776491e741cfa3b
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-10-22 14:54:23 -0300

    add new Pill component

---
+++ components/common/Pill.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="inline-flex bg-secondary text-white px-4 py-1 rounded-full font-bold tracking-widest">
+        {{ text }}
+    </div>
+</template>
+<script setup>
+
+const props = defineProps({
+    text: String,
+});
+
+</script>
