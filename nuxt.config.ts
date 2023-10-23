dee960996cea8c7541336cafa3b92ef4dd1dbe1b
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-10-22 21:45:59 -0300

    add Nuxt Icon module and remove font awesome integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/google-fonts', 'nuxt-icon'],
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: 'RoomMy',
-      script: [
-        {
-          src: 'https://kit.fontawesome.com/64d58efce2.js',
-          crossorigin: 'anonymous',
-        }
-      ]
     },
     layoutTransition: {
       name: 'layout',
