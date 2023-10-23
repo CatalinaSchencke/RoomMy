1d352f7bf0d9acebdfa31a1104634be903f5ae9c
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-10-22 21:51:29 -0300

    add social media icons to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="flex gap-x-4 justify-center text-secondary">
-        <a target="_blank" href="https://www.facebook.com/roommycl">Facebook</a>
-        <a target="_blank" href="https://www.instagram.com/roommycl/">Instagram</a>
+    <div class="flex gap-x-4 justify-center items-center text-secondary">
+        <a target="_blank" href="https://www.facebook.com/roommycl">
+            <Icon name="ion:social-facebook" class="text-3xl" />
+        </a>
+        <a target="_blank" href="https://www.instagram.com/roommycl/">
+            <Icon name="ion:logo-instagram" class="text-2xl" />
+        </a>
     </div>
 </template>
