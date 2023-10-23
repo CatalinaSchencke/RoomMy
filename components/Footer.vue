372dc3ef2bea1172d8bc07d550a4510405e0346e
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-10-22 21:43:56 -0300

    add new very basic footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-
+    <div class="flex gap-x-4 justify-center text-secondary">
+        <a target="_blank" href="https://www.facebook.com/roommycl">Facebook</a>
+        <a target="_blank" href="https://www.instagram.com/roommycl/">Instagram</a>
     </div>
 </template>
