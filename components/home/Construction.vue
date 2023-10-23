b3d51ad4c36b0b299cbc4051f2ed2ba46df55717
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-10-22 21:44:41 -0300

    add new Construction and funcionamiento components

---
+++ components/home/Construction.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="grid grid-cols-2">
+        <div>
+            <CommonPill class="inline-flex py-1 px-4 mb-4 bg-secondary">
+                <p>En construcción</p>
+            </CommonPill>
+            <h2 class="text-6xl text-primary-700 font-bold leading-tight">¡Nos pillaste desprevenidos!</h2>
+            <div class="my-4 leading-loose text-tertiary">
+                <p>Aún estamos trabajando para brindar la mejor solución del sector de vivienda.</p>
+                <p>Mientras tanto, ¿quieres conocer más de nosotros?</p>
+            </div>
+            <div class="flex flex-col">
+                <a href="#">
+                    <CommonPill class="mt-4 bg-primary py-3">
+                        <p>Ver prototipo de RoomMy</p>
+                    </CommonPill>
+                </a>
+                <nuxt-link to="/funcionamiento">
+                    <CommonPill class="mt-4 bg-primary py-3">
+                        <p>Conoce como funcionaremos</p>
+                    </CommonPill>
+                </nuxt-link>
+            </div>
+            <div class="mt-16 font-bold text-primary-700 text-center">
+                <h3 class="text-xl">¿Nos quieres ayudar a brindar la mejor solución?</h3>
+                <p>Te invitamos a responder alguna de las siguientes encuestas:</p>
+            </div>
+            <div class="flex flex-col items-center gap-y-3 mt-5">
+                <a href="#">
+                    <CommonPill class="inline-flex bg-secondary py-2 px-10">
+                        <p>
+                            Busco compañeros
+                            <br />
+                            de vivienda
+                        </p>
+                    </CommonPill>
+                </a>
+                <a href="#">
+                    <CommonPill class="inline-flex bg-secondary py-2 px-10">
+                        <p>
+                            Busco arrendar
+                            <br />
+                            una vivienda
+                        </p>
+                    </CommonPill>
+                </a>
+            </div>
+        </div>
+        <div>
+            <img src="/girl-standing-questioning-2.png" class="w-9/12 ml-auto" alt="girl standing" />
+            <form class="mt-7 w-9/12 mx-auto" @submit.prevent>
+                <div class="flex space-x-2">
+                    <input class="col-span-3 w-full border border-gray-400 px-4 py-3 rounded-md placeholder:text-tertiary"
+                        type="text" placeholder="Correo electrónico" />
+                    <CommonButton class="py-3" text="Suscribirse" />
+                </div>
+                <p class="text-sm mt-3 text-tertiary leading-relaxed">Registra tu correo si quieres ser notificado en cuanto
+                    estemos listos.
+                    <br />
+                    ¡Sé de los primeros en probar esta tecnología revolucionaria!
+                </p>
+            </form>
+        </div>
+    </div>
+</template>
---
+++ pages/funcionamiento.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="px-28 py-10">
+        <CommonPill class="inline-flex py-1 px-4 bg-secondary">
+            <p>En construcción</p>
+        </CommonPill>
+        <h2 class="mt-5 text-primary-700 text-4xl font-bold">¿Qué es RoomMy?</h2>
+        <div class="mt-4 space-y-4 text-tertiary">
+            <p>
+                RoomMy es una plataforma web que utiliza <b>Inteligencia Artificial</b> para facilitar la búsqueda de
+                <b>compañeros de vivienda</b> y arriendos que se ajusten a tus preferencias.
+            </p>
+            <p> Con RoomMy, puedes encontrar personas afines a ti, con gustos, presupuesto y en busca de propiedades de
+                ubicación similar para compartir un espacio cómodo y seguro. Además, podrás acceder a una amplia oferta de
+                arriendos que cumplan con tus criterios de vivienda y recibir asesoría personalizada para elegir la mejor
+                opción.
+            </p>
+            <p>
+                Si eres quien está ofreciendo el arriendo de una propiedad, también pensamos en ti. En RoomMy, podrás
+                definir
+                tus criterios de arrendatario, presupuesto y más, para así encontrar al mejor ocupante de tu propiedad.</p>
+            <p>
+                RoomMy te brinda confianza y tranquilidad, ya que todos los perfiles son verificados y supervisados por los
+                administradores de la plataforma.
+            </p>
+        </div>
+        <h3 class="text-primary-700 text-2xl font-bold text-center my-10">¿Estás buscando compañeros de vivienda?</h3>
+        <div class="space-y-4 text-tertiary">
+            <p>
+                Ya sea por tener un presupuesto acotado u otra razón, compartir un arriendo puede llegar a ser una buena
+                opción.
+                Sin embargo, es importante conocer a la otra persona, sus preferencias y asegurarse de que la convivencia
+                sea
+                cómoda y segura.
+                <br />
+                En RoomMy, nos aseguramos de que esta instancia sea lo más segura, accesible y cómoda posible.
+            </p>
+            <p>
+                Con RoomMy, podrás realizar:
+            </p>
+            <ul class="list-disc pl-10">
+                <li>
+
+                    Crear un perfil verificado con tu documento de identificación: No dejaremos pie a usurpación de
+                    identidad y
+                    perfiles falsos.
+
+                </li>
+                <li>
+                    Crear un perfil con tus preferencias, fotografía (opcional) y una pequeña biografía: Con esto,
+                    conoceremos
+                    un poco más de ti para mostrarte los compañeros de vivienda más compatibles.
+                </li>
+                <li>
+                    Visualizar, enviar y recibir solicitudes de otros perfiles: En RoomMy, podrás acceder a una
+                    recomendación de
+                    perfiles en base a la mayor compatibilidad entre sí. Podrás saber el porcentaje exacto de compatibilidad
+                    y
+                    visitar el perfil de tu posible compañero de vivienda.
+                </li>
+            </ul>
+        </div>
+        <div class="text-center my-10">
+            <div class="text-primary-700 font-bold mb-3">
+                <h4 class="text-xl">¿Nos quieres ayudar a brindar la mejor solución?</h4>
+                <p>Te invitamos a responder la siguiente encuesta:</p>
+            </div>
+            <a href="#">
+                <CommonPill class="inline-flex px-10 py-2 bg-secondary">
+                    <p>
+                        Buscaré compañeros
+                        <br />
+                        de vivienda
+                    </p>
+                </CommonPill>
+            </a>
+        </div>
+        <h3 class="text-primary-700 text-2xl font-bold text-center my-10">¿Estás buscando el arriendo ideal?</h3>
+        <div class="space-y-4 text-tertiary">
+            <p>
+                Ya sea que busques solo o con más personas, encontrar el arriendo perfecto que se ajuste a tus necesidades y
+                preferencias puede ser difícil. En RoomMy, nos aseguramos de que esta instancia sea lo más segura, accesible
+                y personalizada posible.
+            </p>
+            <p>
+                Con RoomMy, podrás realizar:
+            </p>
+            <ul class="list-disc pl-10">
+                <li>
+                    Crear un perfil verificado con tu documento de identificación: No dejaremos pie a usurpación de
+                    identidad y perfiles falsos.
+                </li>
+                <li>
+                    Crear un perfil con tus preferencias y una pequeña biografía: Con esto, conoceremos un poco más de ti (o
+                    ustedes en caso de buscar con más personas) para mostrarte las mejores y más adecuadas publicaciones de
+                    arriendo para ti.
+                </li>
+                <li>
+                    Enviar y recibir información de las publicaciones que te interesen: En RoomMy, podrás acceder a una
+                    recomendación de publicaciones de arriendo en base a la mayor compatibilidad entre sí. Podrás saber el
+                    porcentaje exacto de compatibilidad y visitar la publicación. Si te interesa, puedes solicitar más
+                    información. Quien publica un arriendo también puede ver recomendaciones de sus clientes ideales, por lo
+                    que puedes recibir información sin costo alguno y decidir si quieres que te contacten (hacer visible tu
+                    información de contacto para ellos, así puedan comunicarse).
+                </li>
+            </ul>
+        </div>
+        <h3 class="text-primary-700 text-2xl font-bold text-center my-10">¿Estás buscando el arrendatario ideal?</h3>
+        <div class="space-y-4 text-tertiary">
+            <p>
+                El buscar quien cumpla con todos los requisitos y cualidades que buscas entre potenciales arrendatarios es
+                una ardua tarea. ¿Cómo saber si son personas reales?, ¿si cumplen los requisitos?, ¿cuidarán la propiedad?
+            </p>
+            <p>
+                En RoomMy, nos aseguramos de que este camino no sea una mala experiencia, procurando que sea una valiosa
+                relación con un arrendatario lo más cercano a lo ideal.
+            </p>
+            <p>
+                Con RoomMy, podrás realizar:
+            </p>
+            <ul class="list-disc pl-10">
+                <li>
+                    Crear un perfil verificado con tu documento de identificación: No dejaremos pie a usurpación de
+                    identidad y perfiles falsos.
+                </li>
+                <li>
+                    Publicar una o más propiedades, donde puedes requerir diferentes cualidades a los posibles
+                    arrendatarios.
+                </li>
+                <li>
+                    Visualizar perfiles de posibles interesados compatibles con tu publicación: En RoomMy, podrás acceder a
+                    una recomendación de perfiles en base a la mayor compatibilidad entre sí. Podrás visitar los perfiles
+                    recomendados y visualizar cuánta compatibilidad tienen con tu publicación.
+                </li>
+                <li>
+                    Recibir solicitudes de información: Puedes visualizar la compatibilidad de quien consulta y decidir si
+                    enviar la información de contacto.
+            </li>
+        </ul>
+    </div>
+</div></template>
