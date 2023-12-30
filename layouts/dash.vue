26d2365a2cde3df6a0975f1dbff04933c7eb710f
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-12-30 17:45:13 -0300

    remove old code and assets

--- layouts/dash.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <DashboardHeader />
-        <slot />
-        <Footer />
-    </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="px-6  sm:px-10 lg:pl-28 lg:pr-0 lg:py-5">
-        <HomeConstruction />
+    <div class="px-6 sm:px-10 lg:pl-28 lg:pr-0 lg:py-5">
+        <HomeLanding />
     </div>
 </template>
