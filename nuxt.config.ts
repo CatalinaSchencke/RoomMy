8a6552bc6c226422845a52520f00ba48531d9f1c
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2023-12-30 19:05:01 -0300

    add nuxt img module to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/google-fonts', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/google-fonts', 'nuxt-icon', '@nuxt/image'],
   app: {
     head: {
       charset: 'utf-8',
